30e6b3120bcf1034687b95e10616e72bd5203863
seokiis <cat1181123@naver.com>
2023-01-17 19:59:54 +0900

    last commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+import { LocaleProvider } from "./contexts/LocaleContext";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <LocaleProvider defaultValue="ko">
+        <App />
+    </LocaleProvider>
+);
